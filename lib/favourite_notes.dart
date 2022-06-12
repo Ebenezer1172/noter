d96055cf86834af8adf52147682feeed63a62aa3
ebenezer1172 <ebencoco1172@gmail.com>
2022-06-11 22:01:04 -0700

    qwerty

--- lib/favourite_notes.dart
+++ lib/favourite_notes.dart
@@ -1,20 +1,23 @@
-import 'dart:math'; 
+import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:noter/note_provider.dart';
- 
-class Favourites extends StatelessWidget { 
-  const  Favourites({Key? key}) : super(key: key);
+
+class Favourites extends StatelessWidget {
+  const Favourites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<UserNotifier>(context);
     // final noteList = provider.userListFavourites;
- bool isDone =false;
- 
+    bool isDone = false;
+
     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection('Noter').where('isDone').snapshots(),
+        stream: FirebaseFirestore.instance
+            .collection('Noter')
+            .where('id')
+            .snapshots(),
         builder: (
           context,
           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
@@ -44,68 +47,73 @@ class Favourites extends StatelessWidget {
                       color: Colors
                           .primaries[Random().nextInt(Colors.primaries.length)],
                       child: Expanded(
-                          child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        IconButton(onPressed:  (){}, icon:const Icon(Icons.favorite_outline),),
-                                       Checkbox(
-                                        value:
-                                        //  isDone
-                                            note['isDone'] ??=false  
-                                        ,
-                                        onChanged: (_) {
-                                          provider = Provider.of<UserNotifier>(
-                                              context,
-                                              listen: false);
-                                           isDone =
-                                          provider.toggleUserStatus( 
-                                             note['isDone'] ??=false  ,
-                                            );
-                                        }),
-                                        const SizedBox(
-                                          width: 1,
-                                        ),
-                                        IconButton(
-                                          onPressed: () {
-                                            provider.deleteUser(index);
-                                          },
-                                          icon:
-                                              const Icon(Icons.delete_forever),
-                                        ),
-                                      ]),
-                                  Text(
-                                    note['title'] ??= "",
-                                    softWrap: true,
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                                Icons.favorite_outline),
+                                          ),
+                                          Checkbox(
+                                              value:
+                                                  //  isDone
+                                                  note['isDone'] ??= false,
+                                              onChanged: (_) {
+                                                provider =
+                                                    Provider.of<UserNotifier>(
+                                                        context,
+                                                        listen: false);
+                                                isDone =
+                                                    provider.toggleUserStatus(
+                                                  note['isDone'] ??= false,
+                                                );
+                                              }),
+                                          const SizedBox(
+                                            width: 1,
+                                          ),
+                                          IconButton(
+                                            onPressed: () {
+                                              provider.deleteUser(index);
+                                            },
+                                            icon: const Icon(
+                                                Icons.delete_forever),
+                                          ),
+                                        ]),
+                                    Text(
+                                      note['title'] ??= "",
+                                      softWrap: true,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    height: 2,
-                                  ),
-                                  Text(
-                                    note['description'] ??= "",
-                                    style: const TextStyle(fontSize: 15),
-                                  ),
-                                ],
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    Text(
+                                      note['description'] ??= "",
+                                      style: const TextStyle(fontSize: 15),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      )),
+                          ],
+                        ),
+                      ),
                     ),
                   );
                 });
